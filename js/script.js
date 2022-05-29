92c71ec0dedb31a064a282202589a617906419be
Travis Williams <traviswilliams997@gmail.com>
2022-05-28 22:20:19 -0500

    Add hover event listener

--- js/script.js
+++ js/script.js
@@ -21,11 +21,34 @@ for(let a = 1; a <= 16; a++ ){
 
     for(let i = 1; i <= 16; i++){
         const col = document.createElement('div');
-        col.classList.add(`row1col${i}`);
+        col.classList.add('col',`row1col${i}`);
         cols.appendChild(col);
     }
 
 }
+
+// to hover when mouuse over
+const hover = document.querySelectorAll('.col');
+
+for (let i = 0; i < hover.length; i++) {
+    hover[i].addEventListener("mouseover", function() {
+        hover[i].setAttribute("style", "background-color: black;") 
+    });
+}
+
+
+// hover.addEventListener("mouseover", func, false);
+// hover.addEventListener("mouseout", func1, false);
+
+// function func()
+// {  
+//    hover.setAttribute("style", "background-color: black;")
+// }
+
+// function func1()
+// {  
+//    hover.setAttribute("style", "background-color: white;")
+// }
 
 
 
