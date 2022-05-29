54c976e2576c742d7c9e8e0f79cfaad43c212afe
Travis Williams <traviswilliams997@gmail.com>
2022-05-28 21:10:35 -0500

    Replace 16 seprate column intializations with a loop

--- js/script.js
+++ js/script.js
@@ -1,105 +1,77 @@
 const container = document.querySelector('#container');
 
 
-
-
+//initialize 16 rows and add to main container div 
 const row1 = document.createElement('div');
-row1.classList.add('#row1');
+row1.classList.add('row1', 'row');
 container.appendChild(row1);
 const row2 = document.createElement('div');
-row2.classList.add('#row2');
+row2.classList.add('row2', 'row');
 container.appendChild(row2);
 const row3 = document.createElement('div');
-row3.classList.add('#row3');
+row3.classList.add('row3', 'row');
 container.appendChild(row3);
 const row4 = document.createElement('div');
-row4.classList.add('#row4');
+row4.classList.add('row4', 'row');
 container.appendChild(row4);
 const row5 = document.createElement('div');
-row5.classList.add('#row5');
+row5.classList.add('row5', 'row');
 container.appendChild(row5);
 const row6 = document.createElement('div');
-row6.classList.add('#row6');
+row6.classList.add('row6', 'row');
 container.appendChild(row6);
 const row7 = document.createElement('div');
-row7.classList.add('#row7');
+row7.classList.add('row7', 'row');
 container.appendChild(row7);
 const row8 = document.createElement('div');
-row8.classList.add('#row8');
+row8.classList.add('row8', 'row');
 container.appendChild(row8);
 const row9 = document.createElement('div');
-row9.classList.add('#row9');
+row9.classList.add('row9', 'row');
 container.appendChild(row9);
 const row10 = document.createElement('div');
-row10.classList.add('#row10');
+row10.classList.add('row10', 'row');
 container.appendChild(row10);
 const row11 = document.createElement('div');
-row11.classList.add('#row11');
+row11.classList.add('row11', 'row');
 container.appendChild(row11);
 const row12 = document.createElement('div');
-row12.classList.add('#row12');
+row12.classList.add('row12', 'row');
 container.appendChild(row12);
 const row13 = document.createElement('div');
-row13.classList.add('#row13');
+row13.classList.add('row13', 'row');
 container.appendChild(row13);
 const row14 = document.createElement('div');
-row14.classList.add('#row14');
+row14.classList.add('row14', 'row');
 container.appendChild(row14);
 const row15 = document.createElement('div');
-row15.classList.add('#row15');
+row15.classList.add('row15', 'row');
 container.appendChild(row15);
 const row16 = document.createElement('div');
-row16.classList.add('#row16');
+row16.classList.add('row16', 'row');
 container.appendChild(row16);
 
-const row1col1 = document.createElement('div');
-row1col1.classList.add('#row1col1');
-row1.appendChild(row1col1);
-const row1col2 = document.createElement('div');
-row1col2.classList.add('#row1col2');
-row1.appendChild(row1col2);
-const row1col3 = document.createElement('div');
-row1col3.classList.add('#row1col3');
-row1.appendChild(row1col3);
-const row1col4 = document.createElement('div');
-row1col4.classList.add('#row1col4');
-row1.appendChild(row1col4);
-const row1col5 = document.createElement('div');
-row1col5.classList.add('#row1col5');
-row1.appendChild(row1col5);
-const row1col6 = document.createElement('div');
-row1col6.classList.add('#row1col6');
-row1.appendChild(row1col6);
-const row1col7 = document.createElement('div');
-row1col7.classList.add('#row1col7');
-row1.appendChild(row1col7);
-const row1col8 = document.createElement('div');
-row1col8.classList.add('#row1col8');
-row1.appendChild(row1col8);
-const row1col9 = document.createElement('div');
-row1col9.classList.add('#row1col9');
-row1.appendChild(row1col9);
-const row1col10 = document.createElement('div');
-row1col10.classList.add('#row1col10');
-row1.appendChild(row1col10);
-const row1col11 = document.createElement('div');
-row1col11.classList.add('#row1col11');
-row1.appendChild(row1col11);
-const row1col12 = document.createElement('div');
-row1col12.classList.add('#row1col12');
-row1.appendChild(row1col12);
-const row1col13 = document.createElement('div');
-row1col13.classList.add('#row1col13');
-row1.appendChild(row1col13);
-const row1col14 = document.createElement('div');
-row1col14.classList.add('#row1col14');
-row1.appendChild(row1col14);
-const row1col15 = document.createElement('div');
-row1col15.classList.add('#row1col15');
-row1.appendChild(row1col15);
-const row1col16 = document.createElement('div');
-row1col16.classList.add('#row1col16');
-row1.appendChild(row1col16);
+
+//makes columns stay inline for each row
+const allRows = document.querySelectorAll('div.row').forEach(element => {
+    element.style.display = 'flex';
+    
+});
+
+
+//adds 16 columns to each row 
+for(let a = 1; a <= 16; a++ ){
+    const cols = document.querySelector(`div.row${a}`);
+
+    for(let i = 1; i <= 16; i++){
+        const col = document.createElement('div');
+        col.classList.add(`row1col${i}`);
+        cols.appendChild(col);
+    }
+
+}
+
+
 
 
 
