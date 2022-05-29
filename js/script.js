4dca18c4a0a36c8ba054ff55608e5f69ca3cb252
Travis Williams <traviswilliams997@gmail.com>
2022-05-28 19:32:16 -0500

    Add 16 rows and 16 columns to first row

---
+++ js/script.js
@@ -0,0 +1,135 @@
+const container = document.createElement('div');
+container.classList.add('#container');
+const row1 = document.createElement('div');
+row1.classList.add('#row1');
+container.appendChild(row1);
+const row2 = document.createElement('div');
+row2.classList.add('#row2');
+container.appendChild(row2);
+const row3 = document.createElement('div');
+row3.classList.add('#row3');
+container.appendChild(row3);
+const row4 = document.createElement('div');
+row4.classList.add('#row4');
+container.appendChild(row4);
+const row5 = document.createElement('div');
+row5.classList.add('#row5');
+container.appendChild(row5);
+const row6 = document.createElement('div');
+row6.classList.add('#row6');
+container.appendChild(row6);
+const row7 = document.createElement('div');
+row7.classList.add('#row7');
+container.appendChild(row8);
+const row8 = document.createElement('div');
+row8.classList.add('#row8');
+container.appendChild(row8);
+const row9 = document.createElement('div');
+row9.classList.add('#row9');
+container.appendChild(row9);
+const row10 = document.createElement('div');
+row10.classList.add('#row10');
+container.appendChild(row10);
+const row11 = document.createElement('div');
+row11.classList.add('#row11');
+container.appendChild(row11);
+const row12 = document.createElement('div');
+row12.classList.add('#row12');
+container.appendChild(row12);
+const row13 = document.createElement('div');
+row13.classList.add('#row13');
+container.appendChild(row13);
+const row14 = document.createElement('div');
+row14.classList.add('#row14');
+container.appendChild(row14);
+const row15 = document.createElement('div');
+row15.classList.add('#row15');
+container.appendChild(row15);
+const row16 = document.createElement('div');
+row16.classList.add('#row16');
+container.appendChild(row16);
+
+const row1col1 = document.createElement('div');
+row1col1.classList.add('#row1col1');
+row1.appendChild(row1col1);
+const row1col2 = document.createElement('div');
+row1col2.classList.add('#row1col2');
+row1.appendChild(row1col2);
+const row1col3 = document.createElement('div');
+row1col3.classList.add('#row1col3');
+row1.appendChild(row1col3);
+const row1col4 = document.createElement('div');
+row1col4.classList.add('#row1col4');
+row1.appendChild(row1col4);
+const row1col5 = document.createElement('div');
+row1col5.classList.add('#row1col5');
+row1.appendChild(row1col5);
+const row1col6 = document.createElement('div');
+row1col6.classList.add('#row1col6');
+row1.appendChild(row1col6);
+const row1col7 = document.createElement('div');
+row1col7.classList.add('#row1col7');
+row1.appendChild(row1col7);
+const row1col8 = document.createElement('div');
+row1col8.classList.add('#row1col8');
+row1.appendChild(row1col8);
+const row1col9 = document.createElement('div');
+row1col9.classList.add('#row1col9');
+row1.appendChild(row1col9);
+const row1col10 = document.createElement('div');
+row1col10.classList.add('#row1col10');
+row1.appendChild(row1col10);
+const row1col11 = document.createElement('div');
+row1col11.classList.add('#row1col11');
+row1.appendChild(row1col11);
+const row1col12 = document.createElement('div');
+row1col12.classList.add('#row1col12');
+row1.appendChild(row1col12);
+const row1col13 = document.createElement('div');
+row1col13.classList.add('#row1col13');
+row1.appendChild(row1col13);
+const row1col14 = document.createElement('div');
+row1col14.classList.add('#row1col14');
+row1.appendChild(row1col14);
+const row1col15 = document.createElement('div');
+row1col15.classList.add('#row1col15');
+row1.appendChild(row1col15);
+const row1col16 = document.createElement('div');
+row1col16.classList.add('#row1col16');
+row1.appendChild(row1col16);
+
+
+
+
+
+
+
+    const btnrock = document.querySelector('#rock');
+    
+    
+   
+    const resultscontainer = document.querySelector('#resultscontainer');
+    const scorecontainer = document.querySelector('#scorecontainer');
+  
+    const score = document.createElement('div');
+    score.textContent = ` Player score is ${ playerScore }. Computer  score is ${ computerScore }. `;
+    scorecontainer.appendChild(score);
+
+    //button for player to choose rock
+    btnrock.addEventListener('click', function() { 
+        const results = document.createElement('div');
+        results.classList.add('results');
+        let whoWon = playRound('ROCK', computerPlay());
+        results.textContent = ` ${ whoWon } `;
+
+      
+            score.textContent = ` Player score is ${ playerScore }. Computer  score is ${ computerScore }. `;
+            scorecontainer.appendChild(score);
+
+
+            resultscontainer.appendChild(results);
+        
+   
+    });
+   
+  
